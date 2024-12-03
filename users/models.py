036e8b32938f1229977b5137392b9a17c0723386
gordoly <gordonly@Gordons-Laptop.local>
2024-12-03 22:28:37 +1100

    created model representing the user with its serialiser

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class AccountUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True, primary_key=True)
+    public_key = models.JSONField(null=True, blank=True)
+    about = models.CharField(max_length=300)
+    
+    REQUIRED_FIELDS = ['first_name', 'last_name']
---
+++ users/serialiser.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import AccountUser
+
+
+class UserSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = AccountUser
+        fields = ['first_name', 'last_name', 'username', 'about', 'password', 'public_key']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
