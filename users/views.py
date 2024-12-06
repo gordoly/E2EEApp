5e7c0bbfd886ca7d88c0488de572de51223df1e4
gordoly <gordonly@Gordons-Laptop.hub>
2024-12-06 23:22:38 +1100

    created views that allow the user to logout and view webpages for signup and login

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponseRedirect
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .serialiser import UserSerialiser
 from .models import AccountUser
@@ -10,10 +10,14 @@ from .models import AccountUser
 class RegisterView(APIView):
     def post(self, request):
         serialiser = UserSerialiser(data=request.data)
+        
         if not serialiser.is_valid():
             return Response({'message': serialiser.errors}, status=400)
+        
         serialiser.save()
-        return HttpResponseRedirect("/auth/signin/")
+        request.session["username"] = request.data['username']
+
+        return Response({'message': "/auth/signin/"}, status=200)
 
 class LoginView(APIView):
     def post(self, request):
@@ -23,15 +27,23 @@ class LoginView(APIView):
         try:
             user = AccountUser.objects.get(username=username)
             if not user.check_password(password):
-                raise Response({'message': 'Account credentials could not be found'}, status=403)
+                return Response({'message': 'Account credentials could not be found'}, status=403)
             request.session["username"] = username
 
-            return HttpResponseRedirect("/friends/")
+            return Response({'message': "/friends/"}, status=200)
         
         except AccountUser.DoesNotExist:
             return Response({'message': 'Account credentials could not be found'}, status=403)
 
 class LogoutView(APIView):
-    def post(self, request):
-        request.session["username"] = ""
-        return HttpResponseRedirect("/auth/signin/")
+    def get(self, request):
+        request.session.flush() 
+        return redirect("/auth/signin/")
+    
+class LoginPageView(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+    
+class SignUpPageView(View):
+    def get(self, request):
+        return render(request, "signup.html", {})
